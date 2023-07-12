d4b5d11cf33121002a8f67598c3d11ada6514e3d
amber-king <amberking@pursuit.org>
2023-07-12 13:14:08 -0400

    Sort catArt by price

--- solution.js
+++ solution.js
@@ -69,7 +69,45 @@ const catArtSortDesginedByA = (catArt) => {
 };
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (priceArt) => {
+  // P - sort the carArt arr by prices
+  // E - not all cases have standard number values for price
+  // D - array
+  // A -
+  // iterate through array - loop or while loop - nested looping
+  // you sort by price based on price value - checking if the price is a Number vale or not
+  // return order as it corresponds to which price is more than the prev. price
+  // C-
+
+  let sorted = true;
+  // while loop used to help with swapping as the prices are looped through possibly
+  while (sorted) {
+    sorted = false;
+    for (let i = 0; i < priceArt.length - 1; i++) {
+      //iterates over the array - 1st iteration
+      // set current & prevevious price conditons
+      let price1 = Number(priceArt[i].price);
+      let price2 = Number(priceArt[i + 1].price);
+      
+      // checking if it's a number or not while checking for current price & next price in the list
+      if (isNaN(price1)) {
+        price1 = Number(10 * priceArt[i].price.slice(-1));
+      }
+      if (isNaN(price2)) {
+        price2 = Number(10 * priceArt[i + 1].price.slice(-1));
+      }
+      
+      // this would swap prices into the correct order based off of if the condition is true
+      if (price1 > price2) {
+        sorted = true;
+        let temp = priceArt[i + 1];
+        priceArt[i + 1] = priceArt[i];
+        priceArt[i] = temp;
+      }
+    }
+  }
+  return priceArt;
+};
 
 // Create your own sort function
 // it should sort in ascending order
