a934e25b800934c6d77691c4b80b84b38536a60f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-13 17:20:37 -0400

    passed ascending order words using merge sort

--- solution.js
+++ solution.js
@@ -26,7 +26,7 @@ function mergeAscending(left, right) {
 }
 
 // sort numbers in descending order
-const sortNumsD = (someNums) => { 
+const sortNumsD = (someNums) => {
   if (someNums.length <= 1) {
     return someNums;
   }
@@ -48,10 +48,43 @@ function mergeDescending(right, left) {
   return [...sortedArray, ...right, ...left];
 }
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (someWords) => {
+  if (someWords.length <= 1) {
+    return someWords;
+  }
+  const half = Math.floor(someWords.length / 2);
+  const left = someWords.slice(0, half);
+  const right = someWords.slice(half);
+
+  return mergeWordsAscending(sortWordsA(left), sortWordsA(right));
+};
+
+function mergeWordsAscending(left, right) {
+  const sortedArray = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArray.push(left.shift());
+    } else {
+      sortedArray.push(right.shift());
+    }
+  }
+  return [...sortedArray, ...left, ...right];
+}
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (someWords) => {
+  if (someWords.length <= 1) {
+    return someWords;
+  }
+
+  const half = Math.floor(someWords.length / 2);
+  const left = someWords.slice(0, half);
+  const right = someWords.slice(half);
+
+  return mergeWordsDescending(sortWordsD(left), sortWordsD(right));
+};
+
+function mergeWordsDescending(left, right) {}
 
 // sort products by name, ascending order case insensitive
 const sortProductNamesA = () => {};
