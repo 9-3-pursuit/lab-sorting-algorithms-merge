ea0e31be8c3521bfaf3ff078d660733c64d43367
Keeanu Rodriguez <keeanu7@gmail.com>
2023-07-13 23:05:30 -0400

    2 test passed

--- solution.js
+++ solution.js
@@ -1,10 +1,51 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const mergeAcending = (left,right) => { 
+  const sortedArray = []
+  while(left.length && right.length){ 
+    if(left[0] < right[0]){
+      sortedArray.push(left.shift())
+    }else {
+      sortedArray.push(right.shift())
+    }
+  }
+  return [...sortedArray, ...left, ...right] 
+} 
+const mergeDecending = (left,right) => { 
+  const sortedArray = []
+  while(left.length && right.length){ 
+    if(left[0] > right[0]){
+      sortedArray.push(left.shift())
+    }else {
+      sortedArray.push(right.shift())
+    }
+  }
+  return [...sortedArray, ...left, ...right] 
+} 
+
+const mergeSort = (arr, type) => {
+  const middle = Math.floor(arr.length/2);
+  if(arr.length <= 1){
+    return arr;
+  }
+  const left = arr.slice(0,middle); 
+  const right = arr.slice(middle);
+  if (type === "acending") {
+    return mergeAcending(mergeSort(left, "acending"), mergeSort(right, "acending"))
+  } else if (type === "decending") {
+    return mergeDecending(mergeSort(left, "decending"), mergeSort(right, "decending"))
+  }
+}
+
+const sortNumsA = (someNums) => {
+  return mergeSort(someNums, "acending");
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (someNums) => {
+  return mergeSort(someNums, "decending");
+};
 
 // sort words in ascending order case sensitive
 const sortWordsA = () => {};
