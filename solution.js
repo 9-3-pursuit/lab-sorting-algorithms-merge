0369f357c86adc8734293ebd90e7931438a79dbb
ryanfodera <ryanfodera@pursuit.org>
2023-07-18 13:05:40 -0400

    first test try

--- solution.js
+++ solution.js
@@ -1,7 +1,9 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = () => { someNums.sort((a, b) => a - b);
+  return someNums;
+};
 
 // sort numbers in descending order
 const sortNumsD = () => {};
